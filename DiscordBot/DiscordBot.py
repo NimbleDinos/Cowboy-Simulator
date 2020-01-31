7ca938027101b36421ba0423e629b4e453bf5e33
JJMaslen <44002981+JJMaslen@users.noreply.github.com>
2020-01-31 23:46:27 +0000

    player class started

--- DiscordBot/DiscordBot.py
+++ DiscordBot/DiscordBot.py
@@ -1,7 +1,11 @@
+# Discord Imports
 import discord
 import discord.ext
 from discord.ext.commands import Bot
 from discord.ext import commands 
+
+# Other Files Imports
+import player
 
 bot_prefix = "!"
 client = commands.Bot(command_prefix=bot_prefix)
@@ -12,6 +16,7 @@ async def on_ready():
 	print("Bot is online")
 	print("Name: Cowboy Simulator")
 	print("TD: {}".format(client.user.id))
+	player.Ptest()
 
 file = open("token.txt", "r")
 token = str(file.read())
---
+++ DiscordBot/player.py
@@ -0,0 +1,27 @@
+def playerTest():
+    print("playerTest")
+
+class player():
+
+    # inventory
+    gold = 0
+    gun = 0
+    boose = 0
+    hat = 0
+    horse = 0
+    lasso = 0
+
+    # stats level
+    Health = 100
+    Shooting = 1
+    Hat = 1
+    Riding = 1
+    Catching = 1
+    Mining = 1
+
+    # stats XP
+    Shooting = 0
+    Hat = 0
+    Riding = 0
+    Catching = 0
+    Mining = 0
