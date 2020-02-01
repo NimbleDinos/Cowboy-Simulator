aa2d2ed6a641f6b19b696e891459ffc0d1793299
JJMaslen <44002981+JJMaslen@users.noreply.github.com>
2020-02-01 19:55:59 +0000

    Started levelling implementation

--- DiscordBot/ability.py
+++ DiscordBot/ability.py
@@ -1,7 +1,11 @@
 def abilityTest():
 	print("abilityTest")
 
+<<<<<<< Updated upstream
 class abilityClass():
+=======
+class AbilityClass:
+>>>>>>> Stashed changes
 	level = 1
 	XP = 0
 	XPGoal = 100
@@ -11,7 +15,14 @@ class abilityClass():
 		if XP > XPGoal:
 			updateXPGoal()
 
+<<<<<<< Updated upstream
 	def updateXPGoal():
 		XPGoal = XPGoal * 1.1
 		XP = 0
 		level += 1
+=======
+	def updateXPGoal(self):
+		self.XPGoal = self.XPGoal * 1.1
+		self.XP = 0
+		self.level += 1
+>>>>>>> Stashed changes
--- DiscordBot/player.py
+++ DiscordBot/player.py
@@ -1,4 +1,5 @@
 import ability
+import random
 
 locationList = ["hull", "lincoln", "sheffield", "corral", "gold-mine", "plains", "river", "shooting-range"]
 
@@ -26,7 +27,44 @@ class playerClass():
     horse = 0
     lasso = 0
     pickaxe = 0
+<<<<<<< Updated upstream
     
+=======
+
+    def panAction(self):
+        chanceToFindGold = 0.1 # Don't replace this one
+        randomNumber = random.uniform(0, 1)
+        if chanceToFindGold > randomNumber:
+            self.gold += 1
+        
+        # random generator to pick ability to level up
+        randomAbility = random.randint(0, 4)
+        if randomAbility == 0:
+            self.Shooting.updateXP()
+        if randomAbility == 1:
+            self.Hattitude.updateXP()
+        if randomAbility == 2:
+            self.Riding.updateXP()
+        if randomAbility == 3:
+            self.Catching.updateXP()
+        if randomAbility == 4:
+            self.Mining.updateXP()
+        
+        print("chanceToFindGold: " + str(chanceToFindGold))
+        print("randomNumber: " + str(randomNumber))
+        print("randomAbility: " + str(randomAbility))
+        
+        print("XP:" +str(self.Shooting.XP))
+        print("XP:" +str(self.Hattitude.XP))
+        print("XP:" +str(self.Riding.XP))
+        print("XP:" +str(self.Catching.XP))
+        print("XP:" +str(self.Mining.XP))
+
+    def mineAction(self):
+        ChanceToFindGold = 0.1 # Replace with maths to calculate chance
+        
+
+>>>>>>> Stashed changes
     # stops health going above 100
     def healthCap():
         if self.health > 100:
@@ -46,6 +84,11 @@ class playerClass():
             return 1
     
     # changes players location when game says they have arrived
+<<<<<<< Updated upstream
     def updateLocation(location):
         self.currcurrentLocation = location
     
+=======
+    def updateLocation(self, location):
+        self.currentLocation = location
+>>>>>>> Stashed changes
