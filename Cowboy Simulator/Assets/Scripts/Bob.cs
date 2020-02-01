339f168ecbde19ae78ac04c91a3984ff2b3fb6dd
Mikymac <mikymacmc@hotmail.co.uk>
2020-02-01 20:38:30 +0000

    Begun the pixellifying

--- Cowboy Simulator/Assets/Scripts/Bob.cs
+++ Cowboy Simulator/Assets/Scripts/Bob.cs
@@ -8,6 +8,15 @@ public class Bob : MonoBehaviour
     public List< GameObject > Players;
     public void CreatePlayer(Join data)
     {
+        foreach(GameObject check in Players)
+        {
+            if (check.name == data.playerId.ToString())
+            {
+                DestroyImmediate(check);
+                break;
+            }
+        }
+
         GameObject Player = Instantiate(playerPrefab);
 
         Player.name = data.playerId.ToString();
