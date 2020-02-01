1077d0343f60b5a4fb0c05d740cab73f8be84ad9
Lottie Bryant <amaliciabryant@gmail.com>
2020-02-01 17:06:51 +0000

    working call for api in game

---
+++ Cowboy Simulator/Assets/Scripts/Join.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Join
+{
+    public int playerId { get; set; }
+    public string playerName { get; set; }
+}
---
+++ Cowboy Simulator/Assets/Scripts/WranglerHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+using Newtonsoft.Json;
+
+public class WranglerHelper : MonoBehaviour
+{
+
+    // Update is called once per frame
+    async void Update()
+    {
+        var join = await GetJoin();
+    }
+
+    async Task<Join> GetJoin()
+    {
+        var response = await new WWW("http://localhost:8081/getJoin");
+        if (!string.IsNullOrEmpty(response.error))
+        {
+            throw new Exception();
+        }
+        var json = response.text;
+        var join = JsonConvert.DeserializeObject<Join>(json);
+        Debug.Log(join.playerId);
+
+        return join;
+    }
+}
