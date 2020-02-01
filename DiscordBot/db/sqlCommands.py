f45d8bb3461e9c6650ad8bbc96da5d4309d557cc
Lottie Bryant <amaliciabryant@gmail.com>
2020-02-01 19:47:19 +0000

    do the thing

--- DiscordBot/db/sqlCommands.py
+++ DiscordBot/db/sqlCommands.py
@@ -1,10 +1,19 @@
 sql_create_users_table = """CREATE TABLE IF NOT EXISTS players (
-                                id INTEGER PRIMARY KEY
+                                id INTEGER PRIMARY KEY,
+                                status INTEGER
                         ); """
 
-sql_insert_player = """INSERT INTO players(id)
-                        VALUES(?) """
+sql_insert_player = """INSERT INTO players(id, status)
+                        VALUES(?, ?) """
 
-sql_select_player = """SELECT 1 
-                        FROM players
-                        WHERE id=?"""
+sql_select_player_exists = """SELECT 1 
+                            FROM players
+                            WHERE id=?"""
+
+sql_select_player_status = """SELECT status
+                                FROM players
+                                WHERE id=?"""
+
+sql_update_player_status = """UPDATE players
+                            SET status=?
+                            WHERE id=?"""
