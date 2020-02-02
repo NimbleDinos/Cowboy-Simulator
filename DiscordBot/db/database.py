c493a6c2fe8589afdeec2fcc51897778e63a4ba4
Lottie Bryant <amaliciabryant@gmail.com>
2020-02-02 08:41:25 +0000

    added new SQL functionality to database

--- DiscordBot/db/database.py
+++ DiscordBot/db/database.py
@@ -48,13 +48,62 @@ class Database:
         rows = cur.fetchall()
         return rows
 
-    def update_player_status(self, player_id, status):
-        cur = self.conn.cursor()
-        cur.execute(db.sqlCommands.sql_update_player_status, (status, player_id))
-        self.conn.commit()
-
     def select_active_players(self, player_id):
         cur = self.conn.cursor()
         cur.execute(db.sqlCommands.sql_select_active_player, (player_id,))
         rows = cur.fetchall()
         return rows
+
+    def select_user_inventory(self, player_id):
+        cur = self.conn.cursor()
+        cur.execute(db.sqlCommands.sql_select_all_inventory, (player_id,))
+        rows = cur.fetchall()
+        return rows
+
+    def select_user_health(self, player_id):
+        cur = self.conn.cursor()
+        cur.execute(db.sqlCommands.sql_select_health, (player_id,))
+        rows = cur.fetchall()
+        return rows
+
+    def select_user_gun(self, player_id):
+        cur = self.conn.cursor()
+        cur.execute(db.sqlCommands.sql_select_gun, (player_id,))
+        rows = cur.fetchall()
+        return rows
+
+    def select_user_booze(self, player_id):
+        cur = self.conn.cursor()
+        cur.execute(db.sqlCommands.sql_select_booze, (player_id,))
+        rows = cur.fetchall()
+        return rows
+
+    def select_user_hat(self, player_id):
+        cur = self.conn.cursor()
+        cur.execute(db.sqlCommands.sql_select_hat, (player_id,))
+        rows = cur.fetchall()
+        return rows
+
+    def select_user_horse(self, player_id):
+        cur = self.conn.cursor()
+        cur.execute(db.sqlCommands.sql_select_horse, (player_id,))
+        rows = cur.fetchall()
+        return rows
+
+    def select_user_lasso(self, player_id):
+        cur = self.conn.cursor()
+        cur.execute(db.sqlCommands.sql_select_lasso, (player_id,))
+        rows = cur.fetchall()
+        return rows
+
+    def select_user_pickaxe(self, player_id):
+        cur = self.conn.cursor()
+        cur.execute(db.sqlCommands.sql_select_pickaxe, (player_id,))
+        rows = cur.fetchall()
+        return rows
+
+    def update_player_status(self, player_id, status):
+        cur = self.conn.cursor()
+        cur.execute(db.sqlCommands.sql_update_player_status, (status, player_id))
+        self.conn.commit()
+
--- DiscordBot/db/sqlCommands.py
+++ DiscordBot/db/sqlCommands.py
@@ -3,6 +3,18 @@ sql_create_users_table = """CREATE TABLE IF NOT EXISTS players (
                                 status INTEGER
                         ); """
 
+sql_create_inventory_table = """CREATE TABLE IF NOT EXISTS inventory (
+                                id INTEGER PRIMARY KEY,
+                                health INTEGER,
+                                gold INTEGER,
+                                gun INTEGER,
+                                booze INTEGER,
+                                hat INTEGER,
+                                horse INTEGER,
+                                lasso INTEGER,
+                                pickaxe INTEGER
+                        );"""
+
 sql_insert_player = """INSERT INTO players(id, status)
                         VALUES(?, ?) """
 
@@ -22,3 +34,48 @@ sql_select_active_player = """SELECT 1
                             FROM players
                             WHERE status=1
                             AND id=?"""
+
+sql_select_all_inventory = """SELECT * 
+                            FROM inventory
+                            WHERE id=?
+                            """
+
+sql_select_gold = """SELECT gold 
+                    FROM inventory
+                    WHERE id=?
+                    """
+
+sql_select_health = """SELECT health 
+                    FROM inventory
+                    WHERE id=?
+                    """
+
+sql_select_gun = """SELECT gun 
+                    FROM inventory
+                    WHERE id=?
+                    """
+
+sql_select_booze = """SELECT booze 
+                    FROM inventory
+                    WHERE id=?
+                    """
+
+sql_select_hat = """SELECT hat 
+                    FROM inventory
+                    WHERE id=?
+                    """
+
+sql_select_horse = """SELECT horse 
+                    FROM inventory
+                    WHERE id=?
+                    """
+
+sql_select_lasso = """SELECT lasso 
+                    FROM inventory
+                    WHERE id=?
+                    """
+
+sql_select_pickaxe = """SELECT pickaxe 
+                    FROM inventory
+                    WHERE id=?
+                    """
